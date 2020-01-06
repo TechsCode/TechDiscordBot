861e84d91246d9c5a909e6aeb85642f706aa05c4
TheTechmo <techmomaster@gmail.com>
2020-01-06 17:35:15 +1300

    Fixed Eazy's fucked up regex

--- src/main/java/me/TechsCode/TechDiscordBot/modules/TicketSystem.java
+++ src/main/java/me/TechsCode/TechDiscordBot/modules/TicketSystem.java
@@ -417,7 +417,7 @@ public class TicketSystem extends Module {
     }
 
     public TextChannel createTicketChannel(Member member) {
-        String name = "ticket-" + member.getEffectiveName().replace(" ", "").replaceAll("^[a-zA-Z0-9_-]*$", "").toLowerCase();
+        String name = "ticket-" + member.getEffectiveName().replaceAll("[^a-zA-Z\d\s_-]+", "").toLowerCase();
         if(name.equals("ticket-")) name = "ticket-" + member.getUser().getId();
         return (TextChannel) bot.getGuild().getController().createTextChannel(name)
                 .setParent(UNRESPONDED_TICKETS_CATEGORY.query().first())
