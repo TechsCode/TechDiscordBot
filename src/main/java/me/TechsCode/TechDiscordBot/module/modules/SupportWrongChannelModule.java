71890b9073991a6b52034cd9fa12101fb35217d7
EazyFTW <Evan.Zimmerman@comcast.net>
2020-07-04 22:30:03 -0500

    SupportWrongChannelModule update.

--- src/main/java/me/TechsCode/TechDiscordBot/module/modules/SupportWrongChannelModule.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/modules/SupportWrongChannelModule.java
@@ -1,10 +1,14 @@
 package me.TechsCode.TechDiscordBot.module.modules;
 
+import me.TechsCode.SpigotAPI.client.collections.PurchaseCollection;
+import me.TechsCode.SpigotAPI.client.objects.Purchase;
 import me.TechsCode.TechDiscordBot.TechDiscordBot;
 import me.TechsCode.TechDiscordBot.module.Module;
+import me.TechsCode.TechDiscordBot.mysql.storage.Verification;
 import me.TechsCode.TechDiscordBot.objects.DefinedQuery;
 import me.TechsCode.TechDiscordBot.objects.Query;
 import me.TechsCode.TechDiscordBot.objects.Requirement;
+import me.TechsCode.TechDiscordBot.util.Plugin;
 import me.TechsCode.TechDiscordBot.util.TechEmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -15,6 +19,8 @@ import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class SupportWrongChannelModule extends Module {
 
@@ -53,10 +59,12 @@ public class SupportWrongChannelModule extends Module {
 
     @SubscribeEvent
     public void onMessage(GuildMessageReceivedEvent e) {
-        if(e.getMember() == null) return;
-        if(e.getAuthor().isBot()) return;
-        if(e.getChannel().getId().equals(GENERAL_CHANNEL.query().first().getId()) || e.getChannel().getId().equals(PLUGIN_DISCUSSION_CHANNEL.query().first().getId()) || e.getChannel().getId().equals(CODING_HELP.query().first().getId())) {
-            if(Arrays.stream(triggerWords).anyMatch(word -> e.getMessage().getContentDisplay().toLowerCase().contains(word))) {
+        if (e.getMember() == null) return;
+        if (e.getAuthor().isBot()) return;
+        if (e.getMember().getRoles().stream().anyMatch(r -> r.getName().equalsIgnoreCase("Staff"))) return;
+
+        if (e.getChannel().getId().equals(GENERAL_CHANNEL.query().first().getId()) || e.getChannel().getId().equals(PLUGIN_DISCUSSION_CHANNEL.query().first().getId()) || e.getChannel().getId().equals(CODING_HELP.query().first().getId())) {
+            if (Arrays.stream(triggerWords).anyMatch(word -> e.getMessage().getContentDisplay().toLowerCase().contains(word))) {
                 triggerMessage(e.getChannel(), e.getMember());
             }
         }
@@ -67,7 +75,7 @@ public class SupportWrongChannelModule extends Module {
         if(e.getUser().isBot()) return;
         if(messages.containsKey(e.getMessageId())) {
             String memberId = e.getMember().getId();
-            if(messages.get(e.getMessageId()).equals(memberId)) {
+            if (messages.get(e.getMessageId()).equals(memberId) || e.getMember().getRoles().stream().anyMatch(r -> r.getName().equalsIgnoreCase("Staff"))) {
                 messages.remove(e.getMessageId());
                 e.getChannel().deleteMessageById(e.getMessageId()).queue();
             } else {
@@ -77,13 +85,50 @@ public class SupportWrongChannelModule extends Module {
     }
 
     public void triggerMessage(TextChannel channel, Member member) {
-        if(messages.containsValue(member.getId())) return;
+        if (messages.containsValue(member.getId())) return;
         TextChannel verificationChannel = bot.getChannel("695493411117072425");
-        Message message = new TechEmbedBuilder().setText("**Hello, " + member.getAsMention() + "!** I've detected that you might be trying to get help in this channel! Please verify in " + verificationChannel.getAsMention() + " in order to get help, thanks!\n\n*If you are not trying to get help, you can delete this message by reacting to it!*")
-                .error()
-                .send(channel);
-        message.addReaction("❌").queue();
-        messages.put(message.getId(), member.getId());
+
+
+        TechEmbedBuilder teb = new TechEmbedBuilder().setText("Hello, " + member.getAsMention() + "! I've detected that you might be trying to get help in this channel! Please verify in " + verificationChannel.getAsMention() + " in order to get help, thanks!\n\n*If you are not trying to get help, you can delete this message by reacting to it!*")
+                .error();
+
+        Message message;
+        Verification verification = TechDiscordBot.getStorage().retrieveVerificationWithDiscord(member);
+        if (verification != null) {
+            PurchaseCollection pc = TechDiscordBot.getSpigotAPI().getPurchases().userId(verification.getUserId());
+            if (pc.size() > 0) {
+                StringBuilder sb = new StringBuilder();
+
+                String plugins = Plugin.getEmotesByList(pc.getStream().map(Purchase::getResourceName).collect(Collectors.toList()));
+                sb.append("Hello, ")
+                        .append(member.getAsMention())
+                        .append("! It looks like you have bought ")
+                        .append(pc.size() == 1 ? "this plugin" : "these plugins")
+                        .append(" already: ")
+                        .append(plugins)
+                        .append("\n\nHere are the corresponding channels:\n\n");
+
+                StringBuilder channels = new StringBuilder();
+                pc.getStream().forEach(p -> {
+                    Plugin plugin = Plugin.byRoleName(p.getResourceName());
+                    channels.append("- ").append(Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById(plugin.getChannelId())).getAsMention()).append("\n");
+                });
+
+                sb.append(channels.toString());
+                sb.append("\nPlease use the corresponding plugin channel above to get support.\nThis channel is **not** a support channel.\n\n*If you are not trying to get help, you can delete this message by reacting to it!*");
+
+                message = new TechEmbedBuilder().setText(sb.toString()).error().send(channel);
+            } else {
+                message = teb.send(channel);
+            }
+        } else {
+            message = teb.send(channel);
+        }
+
+        if (message != null) {
+            message.addReaction("❌").queue();
+            messages.put(message.getId(), member.getId());
+        }
     }
 
     @Override
