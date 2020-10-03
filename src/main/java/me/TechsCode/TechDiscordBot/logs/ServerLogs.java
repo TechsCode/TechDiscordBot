80d3c2dc782eca0d13ed3bedcf2ab49f6b158ff8
EazyFTW <eazyftw@eazyftw.com>
2020-10-03 18:52:50 -0500

    Added EventsModule

--- src/main/java/me/TechsCode/TechDiscordBot/logs/ServerLogs.java
+++ src/main/java/me/TechsCode/TechDiscordBot/logs/ServerLogs.java
@@ -11,12 +11,16 @@ public class ServerLogs {
 
     private static final long CHANNEL_ID = 761382066633572373L;
 
+    public static boolean log(String msg) {
+        return sendChannel("Log", msg, null);
+    }
+
     public static boolean log(String title, String msg) {
         return sendChannel(title, msg, null);
     }
 
-    public static boolean log(String msg) {
-        return sendChannel("Log", msg, null);
+    public static boolean log(TechEmbedBuilder embed) {
+        return sendChannel(embed);
     }
 
     public static boolean error(Exception ex) {
@@ -25,6 +29,16 @@ public class ServerLogs {
 
     public static boolean error(String error) {
         return sendChannel("Error", "```" + error + "```", new Color(178,34,34));
+    }
+
+    private static boolean sendChannel(TechEmbedBuilder embed) {
+        try {
+            embed.send(Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById(CHANNEL_ID)));
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     private static boolean sendChannel(String title, String msg, Color color) {
