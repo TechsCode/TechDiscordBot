e3b7b2243f4f7b1c630e8c8a7ed971903fdbc8ca
EazyFTW <Evan.Zimmerman@comcast.net>
2020-10-15 12:37:28 -0500

    Not specify an argument on the check command not defaults to the user who executed the command.

--- src/main/java/me/TechsCode/TechDiscordBot/module/cmds/UserCheckCommand.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/cmds/UserCheckCommand.java
@@ -44,80 +44,78 @@ public class UserCheckCommand extends CommandModule {
 
     @Override
     public void onCommand(TextChannel channel, Message message, Member m, String[] args) {
+        Member member;
         if(args.length == 0) {
-            new TechEmbedBuilder("Check")
-                    .error()
-                    .setText("Please specify a spigot/user (id) to check!")
-                    .sendTemporary(channel, 10, TimeUnit.SECONDS);
+            member = m;
         } else {
-            Member member = TechDiscordBot.getMemberFromString(message, args[0]);
-
-            if (!TechDiscordBot.getBot().getStatus().isUsable()) {
-                new TechEmbedBuilder("API Not Usable")
-                        .error()
-                        .setText("The API does not contain any information! I cannot check a user if it's offline!")
-                        .sendTemporary(channel, 10, TimeUnit.SECONDS);
-                return;
-            }
-
-            boolean canView = m.getRoles().stream().anyMatch(r -> r.getName().equals("Staff"));
-
-            Verification verification;
-            if(member != null) {
-                verification = TechDiscordBot.getStorage().retrieveVerificationWithDiscord(member.getUser().getId());
-            } else {
-                verification = TechDiscordBot.getStorage().retrieveVerificationWithSpigot(args[0]);
-            }
-
-            if(verification == null) {
-                new TechEmbedBuilder((member == null ? args[0] : member.getEffectiveName()) + " Is Not Verified!")
-                        .setText((member == null ? args[0] : member.getAsMention()) + " has not verified themselves!")
-                        .error().send(channel);
-                return;
-            }
-
-            if(verification.getDiscordId().equals(m.getId()) && !canView)
-                canView = true;
-
-            member = bot.getMember(verification.getDiscordId());
-
-            PurchasesList purchases = TechDiscordBot.getSpigotAPI().getPurchases().userId(verification.getUserId());
-
-            if(!canView) {
-                new TechEmbedBuilder("Not Enough Perms")
-                        .setText("You have to either be Staff or be viewing your self to execute this command!")
-                        .error().send(channel);
-                return;
-            }
-
-            if (member == null || purchases == null || purchases.size() == 0) {
-                new TechEmbedBuilder((member == null ? args[0] : member.getEffectiveName()) + "'s Purchases")
-                        .error()
-                        .setText((member == null ? args[0] : member.getAsMention()) + " has not bought of any Tech's Resources!")
-                        .send(channel);
-                return;
-            }
-
-            Purchase purchase = purchases.stream().sorted(Comparator.comparingLong(p -> p.getTime().getUnixTime())).skip(purchases.size() - 1).findFirst().orElse(null);
-            if (purchase == null) return;
-
-            String date = purchase.getTime().getHumanTime();
-            boolean hasBoughtAll = TechDiscordBot.getSpigotAPI().getResources().premium().size() == purchases.size();
-            StringBuilder sb = new StringBuilder();
-
-            for (Purchase p : purchases)
-                sb.append("- ").append(Plugin.fromId(p.getResource().getId()).getEmoji().getAsMention()).append(" ").append(p.getResource().getName()).append(" ").append(!p.getCost().isPresent() ? "as a Gift/Free" : "for " + p.getCost().get().getValue() + p.getCost().get().getCurrency()).append(" on").append((p.getTime().getHumanTime() != null ? " " + p.getTime().getHumanTime() : " Unknown (*too early to calculate*)")).append(",\n ");
-
-            String purchasesString = sb.toString();
-            new TechEmbedBuilder(member.getEffectiveName())
-                    .success()
-                    .setThumbnail(purchase.getUser().getAvatar())
-                    .setText("Showing " + member.getAsMention() + "'s Spigot Information.")
-                    .addField("Username / ID", "[" + purchase.getUser().getUsername() + "." + purchase.getUser().getUserId() + "](https://www.spigotmc.org/members/" + purchase.getUser().getUsername().toLowerCase() + "." + purchase.getUser().getUserId() + ")", true)
-                    .addField("Purchases Amount", hasBoughtAll ? " **All** " + purchases.size() + " plugins purchased!" : purchases.size() + "**/**" + TechDiscordBot.getSpigotAPI().getResources().premium().size() + " purchased.", true)
-                    .addField("Last Purchase", Plugin.fromId(purchase.getResource().getId()).getEmoji().getAsMention() + " " + (date != null ? date + ".": "Unknown\n*or cannot calculate*."), true)
-                    .addField("Purchases", purchasesString.substring(0, purchasesString.length() - 3) + ".", false)
-                    .send(channel);
+            member = TechDiscordBot.getMemberFromString(message, args[0]);
         }
+
+        if (!TechDiscordBot.getBot().getStatus().isUsable()) {
+            new TechEmbedBuilder("API Not Usable")
+                    .error()
+                    .setText("The API does not contain any information! I cannot check a user if it's offline!")
+                    .sendTemporary(channel, 10, TimeUnit.SECONDS);
+            return;
+        }
+
+        boolean canView = m.getRoles().stream().anyMatch(r -> r.getName().equals("Staff"));
+
+        Verification verification;
+        if(member != null) {
+            verification = TechDiscordBot.getStorage().retrieveVerificationWithDiscord(member.getUser().getId());
+        } else {
+            verification = TechDiscordBot.getStorage().retrieveVerificationWithSpigot(args[0]);
+        }
+
+        if(verification == null) {
+            new TechEmbedBuilder((member == null ? args[0] : member.getEffectiveName()) + " Is Not Verified!")
+                    .setText((member == null ? args[0] : member.getAsMention()) + " has not verified themselves!")
+                    .error().send(channel);
+            return;
+        }
+
+        if(verification.getDiscordId().equals(m.getId()) && !canView)
+            canView = true;
+
+        member = bot.getMember(verification.getDiscordId());
+
+        PurchasesList purchases = TechDiscordBot.getSpigotAPI().getPurchases().userId(verification.getUserId());
+
+        if(!canView) {
+            new TechEmbedBuilder("Not Enough Perms")
+                    .setText("You have to either be Staff or be viewing your self to execute this command!")
+                    .error().send(channel);
+            return;
+        }
+
+        if (member == null || purchases == null || purchases.size() == 0) {
+            new TechEmbedBuilder((member == null ? args[0] : member.getEffectiveName()) + "'s Purchases")
+                    .error()
+                    .setText((member == null ? args[0] : member.getAsMention()) + " has not bought of any Tech's Resources!")
+                    .send(channel);
+            return;
+        }
+
+        Purchase purchase = purchases.stream().sorted(Comparator.comparingLong(p -> p.getTime().getUnixTime())).skip(purchases.size() - 1).findFirst().orElse(null);
+        if (purchase == null) return;
+
+        String date = purchase.getTime().getHumanTime();
+        boolean hasBoughtAll = TechDiscordBot.getSpigotAPI().getResources().premium().size() == purchases.size();
+        StringBuilder sb = new StringBuilder();
+
+        for (Purchase p : purchases)
+            sb.append("- ").append(Plugin.fromId(p.getResource().getId()).getEmoji().getAsMention()).append(" ").append(p.getResource().getName()).append(" ").append(!p.getCost().isPresent() ? "as a Gift/Free" : "for " + p.getCost().get().getValue() + p.getCost().get().getCurrency()).append(" on").append((p.getTime().getHumanTime() != null ? " " + p.getTime().getHumanTime() : " Unknown (*too early to calculate*)")).append(",\n ");
+
+        String purchasesString = sb.toString();
+        new TechEmbedBuilder(member.getEffectiveName())
+                .success()
+                .setThumbnail(purchase.getUser().getAvatar())
+                .setText("Showing " + member.getAsMention() + "'s Spigot Information.")
+                .addField("Username / ID", "[" + purchase.getUser().getUsername() + "." + purchase.getUser().getUserId() + "](https://www.spigotmc.org/members/" + purchase.getUser().getUsername().toLowerCase() + "." + purchase.getUser().getUserId() + ")", true)
+                .addField("Purchases Amount", hasBoughtAll ? " **All** " + purchases.size() + " plugins purchased!" : purchases.size() + "**/**" + TechDiscordBot.getSpigotAPI().getResources().premium().size() + " purchased.", true)
+                .addField("Last Purchase", Plugin.fromId(purchase.getResource().getId()).getEmoji().getAsMention() + " " + (date != null ? date + ".": "Unknown\n*or cannot calculate*."), true)
+                .addField("Purchases", purchasesString.substring(0, purchasesString.length() - 3) + ".", false)
+                .send(channel);
     }
 }
