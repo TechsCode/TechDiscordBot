6ccc036deae8b0cf59a792b0beed0123f5bddec4
EazyFTW <Evan.Zimmerman@comcast.net>
2021-06-01 18:25:53 -0500

    Update JoinModule (Fix mess)

--- src/main/java/me/TechsCode/TechDiscordBot/module/modules/JoinModule.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/modules/JoinModule.java
@@ -12,44 +12,37 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
 public class JoinModule extends Module {
-  private final DefinedQuery<Role> MUTED_ROLE;
-  
-  public JoinModule(TechDiscordBot bot) {
-    super(bot);
-    this.MUTED_ROLE = new DefinedQuery<Role>() {
+
+    private final DefinedQuery<Role> MUTED_ROLE = new DefinedQuery<Role>() {
         protected Query<Role> newQuery() {
-          return JoinModule.this.bot.getRoles(new String[] { "Muted" });
+            return JoinModule.this.bot.getRoles("Muted");
         }
-      };
-  }
-  
-  public void onEnable() {}
-  
-  public void onDisable() {}
-  
-  public String getName() {
-    return "JoinModule";
-  }
-  
-  @SubscribeEvent
-  public void onGuildMemberJoin(GuildMemberJoinEvent event) {
-    Verification existingVerification = TechDiscordBot.getStorage().retrieveVerificationWithDiscord(event.getMember().getId());
-    if (event.getMember().getId().equals(Long.valueOf(547187087770648596L)))
-      event.getGuild().addRoleToMember(event.getMember(), (Role)this.MUTED_ROLE.query().first()).queue(); 
-    if (existingVerification != null) {
-      (new TechEmbedBuilder("Welcome to Tech's Support Server"))
-        .setText("Hello there,\nYou have previously verified yourself, and your roles will be automatically updated as a result.\nThis update may take 10 to 15 minutes to complete.\n")
-        
-        .send(event.getMember());
-      return;
-    } 
-    (new TechEmbedBuilder("Welcome to Tech's Support Server"))
-      .setText("Welcome to Tech's support server,\nIf you are looking for help, you must first verify your spigot acount in <#695493411117072425>.\nAfter you have been verified, you will have access to the support channel(s).\n")
-      
-      .send(event.getMember());
-  }
-  
-  public Requirement[] getRequirements() {
-    return new Requirement[0];
-  }
+    };
+
+    public JoinModule(TechDiscordBot bot) {
+        super(bot);
+    }
+
+    @Override
+    public void onEnable() { }
+
+    @Override
+    public void onDisable() { }
+
+    public String getName() {
+        return "JoinModule";
+    }
+
+    @SubscribeEvent
+    public void onGuildMemberJoin(GuildMemberJoinEvent e) {
+        Verification existingVerification = TechDiscordBot.getStorage().retrieveVerificationWithDiscord(e.getMember().getId());
+
+        new TechEmbedBuilder("Welcome to Tech's Plugin Support")
+                .text((existingVerification != null ? "Welcome to Tech's Plugin Support,\nIf you are looking for help, you must first verify your Spigot Account in <#695493411117072425>.\n\nAfter you have been verified, you will have access to the support channel(s)." : "Hello there,\nYou have previously verified yourself and your roles will be automatically updated as a result.\n\nThis update may take 10 to 15 minutes to complete.") + "\n\nThanks & Welcome,\nTechsCode & Team")
+                .queue(e.getMember());
+    }
+
+    public Requirement[] getRequirements() {
+        return new Requirement[0];
+    }
 }
