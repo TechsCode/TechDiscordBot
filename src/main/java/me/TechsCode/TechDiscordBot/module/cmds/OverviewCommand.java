24c33c8aaef1e987a43313e2f3e34cac4f7e063f
MATRIX <timokueck.email@gmail.com>
2021-07-05 23:17:19 +0200

    Updated Verification Channel ID

--- src/main/java/me/TechsCode/TechDiscordBot/module/cmds/OverviewCommand.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/cmds/OverviewCommand.java
@@ -103,7 +103,7 @@ public class OverviewCommand extends CommandModule {
 
     public void showInfo() {
         new TechEmbedBuilder("Tech's Plugin Support")
-                .text("Welcome to **Tech's Plugin Support**. Here, not only can you get support for Tech's Plugins. You can talk and socialize with people too! You can also get help with other plugins!\n\nIf you're new here and need help with one or more of Tech's Plugins, you can verify in <#416186574078738432> to get support. Once you do, you will get access to the specified support channels.\nIf you are already verified and you have bought another plugin, simply wait for the bot to give you the role *(could take up to 15 minutes, possibly longer)*.")
+                .text("Welcome to **Tech's Plugin Support**. Here, not only can you get support for Tech's Plugins. You can talk and socialize with people too! You can also get help with other plugins!\n\nIf you're new here and need help with one or more of Tech's Plugins, you can verify in <#695493411117072425> to get support. Once you do, you will get access to the specified support channels.\nIf you are already verified and you have bought another plugin, simply wait for the bot to give you the role *(could take up to 15 minutes, possibly longer)*.")
                 .thumbnail("https://i.imgur.com/SfFEnoU.png")
                 .queue(OVERVIEW_CHANNEL.query().first());
     }
