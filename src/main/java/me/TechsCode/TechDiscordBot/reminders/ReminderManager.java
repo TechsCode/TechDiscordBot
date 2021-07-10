b0a26d1d8a3629f5fd05fe1eb7872bbaf8f25a36
Greazi <n.spel.game@gmail.com>
2021-07-10 18:28:40 +0200

    Fix on the remindme command

--- src/main/java/me/TechsCode/TechDiscordBot/reminders/ReminderManager.java
+++ src/main/java/me/TechsCode/TechDiscordBot/reminders/ReminderManager.java
@@ -81,6 +81,7 @@ public class ReminderManager {
                             int timint = Math.abs(Integer.parseInt(tim));
                             time = time + rtt.toMilli(timint);
                             bhb.addX(rtt, timint);
+                            argsAm = argsAm + 2;
                         } catch (Exception e) {
                             e.printStackTrace();
                             return null;
