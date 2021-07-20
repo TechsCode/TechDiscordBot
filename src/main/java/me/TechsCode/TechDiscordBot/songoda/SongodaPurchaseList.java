2438713a92699184ae24624c120dbaef2d82a6aa
EazyFTW <Evan.Zimmerman@comcast.net>
2021-07-20 13:15:06 -0500

    An attempt to fix ConcurrentModificationException.

--- src/main/java/me/TechsCode/TechDiscordBot/songoda/SongodaPurchaseList.java
+++ src/main/java/me/TechsCode/TechDiscordBot/songoda/SongodaPurchaseList.java
@@ -18,27 +18,27 @@ public class SongodaPurchaseList extends ArrayList<SongodaPurchase> {
     }
 
     public SongodaPurchaseList userId(String userId) {
-        return stream().filter(purchase -> purchase.getUser().getUserId().equals(userId)).collect(Collectors.toCollection(SongodaPurchaseList::new));
+        return new SongodaPurchaseList(this).stream().filter(purchase -> purchase.getUser().getUserId().equals(userId)).collect(Collectors.toCollection(SongodaPurchaseList::new));
     }
 
     public SongodaPurchaseList username(String username) {
-        return stream().filter(purchase -> purchase.getUser().getUsername().equalsIgnoreCase(username)).collect(Collectors.toCollection(SongodaPurchaseList::new));
+        return new SongodaPurchaseList(this).stream().filter(purchase -> purchase.getUser().getUsername().equalsIgnoreCase(username)).collect(Collectors.toCollection(SongodaPurchaseList::new));
     }
 
     public SongodaPurchaseList resource(String resourceId) {
-        return stream().filter(purchase -> purchase.getResource().getId().equals(resourceId)).collect(Collectors.toCollection(SongodaPurchaseList::new));
+        return new SongodaPurchaseList(this).stream().filter(purchase -> purchase.getResource().getId().equals(resourceId)).collect(Collectors.toCollection(SongodaPurchaseList::new));
     }
 
     public SongodaPurchaseList gifted() {
-        return stream().filter(p -> p.getCost().isPresent() && p.getCost().get().getValue() == 0F).collect(Collectors.toCollection(SongodaPurchaseList::new));
+        return new SongodaPurchaseList(this).stream().filter(p -> p.getCost().isPresent() && p.getCost().get().getValue() == 0F).collect(Collectors.toCollection(SongodaPurchaseList::new));
     }
 
     public SongodaPurchaseList discord(String discord) {
-        return stream().filter(purchase -> purchase.getDiscord() != null && purchase.getDiscord().equals(discord)).collect(Collectors.toCollection(SongodaPurchaseList::new));
+        return new SongodaPurchaseList(this).stream().filter(purchase -> purchase.getDiscord() != null && purchase.getDiscord().equals(discord)).collect(Collectors.toCollection(SongodaPurchaseList::new));
     }
 
     public SongodaPurchaseList discord(User user) {
-        return stream().filter(purchase -> purchase.getDiscord() != null && purchase.getDiscord().equals(user.getName() + "#" + user.getDiscriminator())).collect(Collectors.toCollection(SongodaPurchaseList::new));
+        return new SongodaPurchaseList(this).stream().filter(purchase -> purchase.getDiscord() != null && purchase.getDiscord().equals(user.getName() + "#" + user.getDiscriminator())).collect(Collectors.toCollection(SongodaPurchaseList::new));
     }
 
     public SongodaPurchaseList discord(Member member) {
