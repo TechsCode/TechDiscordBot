dcea81fe339d4be52c9847e0a0a02acdc1ae7728
Faab007NL <mail@fabiandingemans.nl>
2021-08-24 16:09:28 +0200

    Added pastebin command

---
+++ src/main/java/me/TechsCode/TechDiscordBot/module/cmds/PasteBinCommand.java
@@ -0,0 +1,44 @@
+package me.TechsCode.TechDiscordBot.module.cmds;
+
+import me.TechsCode.TechDiscordBot.TechDiscordBot;
+import me.TechsCode.TechDiscordBot.module.CommandModule;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
+
+public class PasteBinCommand extends CommandModule {
+
+    public PasteBinCommand(TechDiscordBot bot) {
+        super(bot);
+    }
+
+    @Override
+    public String getName() { return "pastebin"; }
+
+    @Override
+    public String getDescription() {
+        return "Returns the pastebin website!";
+    }
+
+    @Override
+    public CommandPrivilege[] getCommandPrivileges() {
+        return new CommandPrivilege[0];
+    }
+
+    @Override
+    public OptionData[] getOptions() {
+        return new OptionData[0];
+    }
+
+    @Override
+    public int getCooldown() {
+        return 5;
+    }
+
+    @Override
+    public void onCommand(TextChannel channel, Member m, SlashCommandEvent e) {
+        e.reply("https://paste.greazi.com").queue();
+    }
+}
