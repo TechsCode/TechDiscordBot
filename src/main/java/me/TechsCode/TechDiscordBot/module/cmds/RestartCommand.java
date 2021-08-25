ca30a0fcc910b21e65d76e4cadb75e587cdbe409
Faab007NL <mail@fabiandingemans.nl>
2021-08-25 22:33:54 +0200

    Added a pissible fix to restart command error

--- src/main/java/me/TechsCode/TechDiscordBot/module/cmds/RestartCommand.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/cmds/RestartCommand.java
@@ -6,17 +6,13 @@ import me.TechsCode.TechDiscordBot.objects.DefinedQuery;
 import me.TechsCode.TechDiscordBot.objects.Query;
 import me.TechsCode.TechDiscordBot.util.Pterodactyl;
 import me.TechsCode.TechDiscordBot.util.TechEmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
 
 import java.awt.*;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -76,33 +72,33 @@ public class RestartCommand extends CommandModule {
 
         if(service.equalsIgnoreCase("Bot")) {
             deleteMessage();
-                e.replyEmbeds(new TechEmbedBuilder("Restart Status Loading...")
-                        .text("Restarting Bot.....")
-                        .color(Color.ORANGE)
-                        .build()
-                ).queue(q ->{
-                    boolean success = Pterodactyl.doRestart();
+            e.replyEmbeds(new TechEmbedBuilder("Restart Status Loading...")
+                    .text("Restarting Bot.....")
+                    .color(Color.ORANGE)
+                    .build()
+            ).queue(q ->{
+                boolean success = Pterodactyl.doRestart();
 
-                    if (success) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-
-                        q.editOriginalEmbeds(new TechEmbedBuilder("Restarted!")
-                                .text("The Bot has been restarted!")
-                                .success()
-                                .build()).queue();
-
-                        Pterodactyl.doKill();
-                    }else{
-                        q.editOriginalEmbeds(new TechEmbedBuilder("Restart Status Failed!")
-                                .text("The Bot failed to restart!")
-                                .error()
-                                .build()).queue();
+                if (success) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
                     }
-                });
+
+                    q.editOriginalEmbeds(new TechEmbedBuilder("Restarted!")
+                            .text("The Bot has been restarted!")
+                            .success()
+                            .build()).queue();
+
+                    Pterodactyl.doKill();
+                }else{
+                    q.editOriginalEmbeds(new TechEmbedBuilder("Restart Status Failed!")
+                            .text("The Bot failed to restart!")
+                            .error()
+                            .build()).queue();
+                }
+            });
         }
         if(service.equalsIgnoreCase("API")) {
             if(member.getRoles().contains(ADMIN_ROLE.query().first())) {
@@ -135,10 +131,23 @@ public class RestartCommand extends CommandModule {
     private void deleteMessage() {
         try {
             List<Message> messages = new ArrayList<>();
-            messages.addAll(Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("695493411117072425")).getHistory().retrievePast(1).complete()); // #📘︱verification
-            messages.addAll(Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("695294630803275806")).getHistory().retrievePast(1).complete()); // #tickets
-            messages.addAll(Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("727403767523442759")).getHistory().retrievePast(1).complete()); // #songoda-transfer
-            messages.addAll(Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("837679014268895292")).getHistory().retrievePast(1).complete()); // #📖︱role-selector
+
+            MessageHistory verificationChannelHistory = Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("695493411117072425")).getHistory(); // #📘︱verification
+            if(!verificationChannelHistory.isEmpty()){
+                messages.addAll(verificationChannelHistory.retrievePast(1).complete());
+            }
+            MessageHistory ticketsChannelHistory = Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("695294630803275806")).getHistory(); // #tickets
+            if(!ticketsChannelHistory.isEmpty()){
+                messages.addAll(ticketsChannelHistory.retrievePast(1).complete());
+            }
+            MessageHistory songodaTransferChannelHistory = Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("727403767523442759")).getHistory(); // #songoda-transfer
+            if(!songodaTransferChannelHistory.isEmpty()){
+                messages.addAll(songodaTransferChannelHistory.retrievePast(1).complete());
+            }
+            MessageHistory roleSelectorChannelHistory = Objects.requireNonNull(TechDiscordBot.getJDA().getTextChannelById("837679014268895292")).getHistory(); // #📖︱role-selector
+            if(!roleSelectorChannelHistory.isEmpty()){
+                messages.addAll(roleSelectorChannelHistory.retrievePast(1).complete());
+            }
 
             messages.forEach(m -> m.delete().complete());
         }catch (Exception e){
@@ -146,5 +155,3 @@ public class RestartCommand extends CommandModule {
         }
     }
 }
-
-
