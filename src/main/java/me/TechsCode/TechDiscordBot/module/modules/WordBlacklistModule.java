bc5a1915e7b582adfd247439a4e2aee99501854d
Faab007NL <mail@fabiandingemans.nl>
2021-12-15 08:57:23 +0100

    Added word blacklist module

--- src/main/java/me/TechsCode/TechDiscordBot/module/modules/WordBlacklistModule.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/modules/WordBlacklistModule.java
@@ -7,6 +7,7 @@ import me.TechsCode.TechDiscordBot.objects.Query;
 import me.TechsCode.TechDiscordBot.objects.Requirement;
 import me.TechsCode.TechDiscordBot.util.TechEmbedBuilder;
 import net.dv8tion.jda.api.entities.Category;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
@@ -18,7 +19,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
@@ -28,7 +28,7 @@ public class WordBlacklistModule extends Module {
     private final DefinedQuery<Category> IGNORED_CATEGORIES = new DefinedQuery<Category>() {
         @Override
         protected Query<Category> newQuery() {
-            return bot.getCategories("\uD83D\uDCC1 | Archives", "\uD83D\uDCD1 | Staff Logs", "staff discussions", "⚖ | Leadership-Discussions");
+            return bot.getCategories("\uD83D\uDCC1 | Archives", "\uD83D\uDCD1 | Staff Logs", "Other Staff Discussions", "staff discussions", "⚖ | Leadership-Discussions");
         }
     };
 
@@ -61,7 +61,7 @@ public class WordBlacklistModule extends Module {
 
     @SubscribeEvent
     public void onMessage(GuildMessageReceivedEvent e) {
-        if (e.getMember() == null || e.getAuthor().isBot() || IGNORED_CATEGORIES.query().stream().anyMatch(c -> c.getId().equals(Objects.requireNonNull(e.getChannel().getParent()).getId()))) return;
+        if (e.getMember() == null || e.getAuthor().isBot() || IGNORED_CATEGORIES.query().stream().anyMatch(c -> c.getId().equals(e.getChannel().getParent().getId()))) return;
 
         if (runMatcher(e.getMessage().getContentRaw().toLowerCase())){
             e.getMessage().delete().queue();
@@ -74,7 +74,7 @@ public class WordBlacklistModule extends Module {
 
     @SubscribeEvent
     public void onMessageUpdate(GuildMessageUpdateEvent e) {
-        if (e.getMember() == null || e.getAuthor().isBot() || IGNORED_CATEGORIES.query().stream().anyMatch(c -> c.getId().equals(Objects.requireNonNull(e.getChannel().getParent()).getId()))) return;
+        if (e.getMember() == null || e.getAuthor().isBot() || IGNORED_CATEGORIES.query().stream().anyMatch(c -> c.getId().equals(e.getChannel().getParent().getId()))) return;
 
         if (runMatcher(e.getMessage().getContentRaw().toLowerCase())){
             e.getMessage().delete().queue();
