bb4b39052ecbb754ca66e8f461dd55b60956f2bf
TehZombiJesus <81379135+TehZombiJesus@users.noreply.github.com>
2022-01-17 07:26:05 -0500

    Modified Server Rules

--- src/main/java/me/TechsCode/TechDiscordBot/module/cmds/RulesCommand.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/cmds/RulesCommand.java
@@ -71,7 +71,9 @@ public class RulesCommand extends CommandModule {
 
     public void showAll() {
         showTOS();
-        showRules();
+        showRules_Section1();
+        showRules_Section2();
+        showRules_Section3();
         showNote();
         showRoles();
     }
@@ -85,39 +87,64 @@ public class RulesCommand extends CommandModule {
                 .queue(OVERVIEW_CHANNEL.query().first());
     }
 
+    public void showRules_Section1() {
+        new TechEmbedBuilder("Section 1 - Text & Voice Chat")
+                .text("**`Article 1.1`**\n" +
+                        "Attempting to bypass any blocked words or links may result in punishments.\n" +
+                        "**`Article 1.2`**\n" +
+                        "Hate speech is strictly prohibited.\n" +
+                        "**`Article 1.3`**\n" +
+                        "NSFW (Not safe for work) is strictly prohibited, resulting in heavy punishments.\n" +
+                        "**`Article 1.4`**\n" +
+                        "Repeating a message five or more times will be classified as spam and prohibited.\n" +
+                        "**`Article 1.5`**\n" +
+                        "Disrespectful, malicious, and harmful messages are strictly prohibited, resulting in heavy punishments.\n" +
+                        "**`Article 1.6`**\n" +
+                        "Mentioning a member then deleting your message is prohibited and may result in punishments.\n" +
+                        "**`Article 1.7`**\n" +
+                        "Mass mentioning users is prohibited and will result in punishments.\n" +
+                        "**`Article 1.8`**\n" +
+                        "Providing any kind of supports to any of Tech's plugins to unverified members is strictly prohibited.")
+                .thumbnail("https://i.imgur.com/SfFEnoU.png")
+                .queue(OVERVIEW_CHANNEL.query().first());
+    }
+
+    public void showRules_Section2() {
+        new TechEmbedBuilder("Section 2 - Advertising")
+                .text("**`Article 2.1`**\n" +
+                        "Advertising anywhere other than your about me and status is prohibited and will result in punishments.\n" +
+                        "**`Article 2.2`**\n" +
+                        "DM Advertising is strictly prohibited and will result in heavy punishments. If a Tech's Plugin Support member directly messages you an advertisement, please create a ticket to report them.\n" +
+                        "**`Article 2.3`**\n" +
+                        "Advertising content such as scams, malware, or NSFW is strictly prohibited and will result in your account being banned.")
+                .thumbnail("https://i.imgur.com/SfFEnoU.png")
+                .queue(OVERVIEW_CHANNEL.query().first());
+    }
+
+    public void showRules_Section3() {
+        new TechEmbedBuilder("Section 3 - Other")
+                .text("**`Article 3.1`**\n" +
+                        "Inappropriate usernames and nicknames are prohibited and result in punishments.\n" +
+                        "**`Article 3.2`**\n" +
+                        "Using leaked or cracked copies of any resource are prohibited and result in heavy punishments.\n" +
+                        "**`Article 3.3`**\n" +
+                        "Using or creating a modified version of premium resources is prohibited, resulting in heavy punishments.\n" +
+                        "**`Article 3.4`**\n" +
+                        "Asking for the staff or other particular roles is prohibited and will result in punishments.\n" +
+                        "**`Article 3.5`**\n" +
+                        "Promoting the use of leaked or cracked resources are strictly prohibited, resulting in heavy punishments.\n" +
+                        "**`Article 3.6`**\n" +
+                        "Inviting or having alternative Discord accounts in this server may result in all of your accounts getting banned unless an Assistant or higher grants permission.\n" +
+                        "**`Article 3.7`**\n" +
+                        "Sharing another user's Personal Identifiable Information (\"PII\") without express permission from them is strictly prohibited and will result in heavy punishments.")
+                .thumbnail("https://i.imgur.com/SfFEnoU.png")
+                .queue(OVERVIEW_CHANNEL.query().first());
+    }
+
     public void showNote() {
         new TechEmbedBuilder()
                 .text("**PLEASE NOTE**: Staff do reserve the right to punish for things not listed on this list under the staff's discretion. Please use common sense, and if you are unsure about anything, please ask.")
                 .queue(OVERVIEW_CHANNEL.query().first());
-    }
-
-    public void showRules() {
-        StringBuilder sBuilder = new StringBuilder();
-
-        List<String> embeds = new ArrayList<>();
-
-        int i = 0;
-        for(Rule rule : Rule.values()) {
-            if((sBuilder.length() + (i == 0? "" : "\n\n") + "- " + rule.getDescription()).length() > 2048) {
-                i = 0;
-                embeds.add(sBuilder.toString());
-                sBuilder = new StringBuilder();
-            }
-
-            sBuilder.append(i == 0 ? "" : "\n\n").append("- ").append(rule.getDescription());
-
-            i++;
-        }
-
-        if(embeds.size() == 0) embeds.add(sBuilder.toString());
-
-        i = 0;
-        for(String s : embeds) {
-            new TechEmbedBuilder(i == 0 ? "Rules" : null, false)
-                    .text(s)
-                    .queue(OVERVIEW_CHANNEL.query().first());
-            i++;
-        }
     }
 
     public void showRoles() {
@@ -138,35 +165,10 @@ public class RulesCommand extends CommandModule {
                 .queue(OVERVIEW_CHANNEL.query().first());
     }
 
-    public enum Rule {
-
-        NSFW("**NSFW content is not allowed** on this server. Keep all content SFW, including profile pictures and names."),
-        SPAMMING("**Do not spam** or else you will be muted/banned. Spam includes posting the same thing across many channels at the same time or spamming characters (letters, numbers, emojis, and symbols)."),
-        INVITES("Server invites **are not allowed** unless it's done privately and with the user's consent."),
-        MASS_MENTION("Do do mass mention a user. If the user has blocked or is ignoring you, it doesn’t count as a proper cause."),
-        GHOST_PING("Ghost pinging a user is prohibited and can result in **mute**."),
-        PINGING_STAFF("Pinging us won't make us answer faster, please be patient and we will get to you once we are available."),
-        SUPPORT_DM("Please do not DM staff for support, instead use the appropriate support channels or create a ticket."),        
-        DOXXING("We have a **zero tolerance policy** for doxxing of any kind. If you're found doxxing **anyone** from the server or others, you will be **banned**."),
-        LAG("Do not purposefully include characters that may crash or lag other devices. Includes massive animated emotes."),
-        MINI_MODDING("Mini-modding, or telling people what to do, is prohibited. If there's something that's not okay, ask a staff member to look into it."),
-        ASKING_FOR_STUFF("Please don’t ask for stuff, including nitro and money. If someone offers to give it to you, that’s fine, but don’t ask for it."),
-        LOGGING("This server logs all deleted and edited messages for in the case of an infraction.");
-
-        private final String description;
-
-        Rule(String description) {
-            this.description = description;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-    }
-
     public enum Role {
 
         CODING_WIZARD("Coding Wizard", "311178859171282944", "It was Tech & now it's MATRIX! The former head of the operations."),
+        DEVELOPER("Developer", "774690360836096062", "These are the Developers! They are in charge of helping the <@&311178859171282944> with the development of our plugins."),
         ASSISTANT("Assistant", "608113993038561325", "This is the Developers Assistant! If the Developers are not online, he is in charge. Occasionally also helps with coding."),
         STAFF("Staff", "608114002387533844", "They are here to help! Don't argue with Staff. If you think there is an issue, please contact <@&608113993038561325> or <@&311178859171282944>."),
         PATREON("Patreon", "795101981051977788", "These are our incredible Patreon supporters who getting rewarded by us for their monthly support with various rewards, additions and exclusive stuff."),
